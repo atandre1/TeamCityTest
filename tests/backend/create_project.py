51d8300b96e837b05ae83863d22d122eb6348224
Andrey Atrahimenok <atandrei@Air-Andrey.realtek>
2024-02-15 13:18:01 +0800

    custom_requester and logger added

--- tests/backend/create_project.py
+++ tests/backend/create_project.py
@@ -12,87 +12,91 @@ class CustomRequestor:
 
 
 class TestProjectCreate:
+    project_data = None
 
     @classmethod
     def setup_class(cls):
-        cls.project_data= ProjectData.create_project_data()
+        cls.project_data = ProjectData.create_project_data()
         cls.create_project_id = cls.project_data["id"]
 
     def test_project_create(self):
-        requester=CustomRequester(requests.Session())
+        requester = CustomRequester(requests.Session())
+        requester.session.auth = ("admin", "admin")
+
+        # requester=CustomRequester(requests.Session())
+        # # Получение токена
+        # auth_response = requests.get(url = f"{BASE_URL}/authenticationTest.html?csrf)", auth=('admin', 'admin'))
+        # csrf_token = auth_response.text
+        # headers = {"X-TC-CSRF-Token": csrf_token}
+        #
+        # #Подготовка данных
+        # project_id ="simpleprojectID5"
+        # conf_id = "simpleconfID2"
+        # project_data={
+        #     "parentProject": {
+        #         "locator": "_Root"
+        #     },
+        #     "name": "ProjectNameSimple5",
+        #     "id": project_id,
+        #     "copyAllAssociatedSettings": True
+        # }
+        # build_data = {
+        #     "id": conf_id,
+        #     "name": "BuildConfName1",
+        #     "project": {
+        #         "id": project_id
+        #     },
+        #     "steps": {
+        #         "step": [
+        #             {
+        #                 "name": "myCommandLineStep",
+        #                 "type": "simpleRunner",
+        #                 "properties": {
+        #                     "property": [
+        #                         {
+        #                             "name": "script.content",
+        #                             "value": "echo 'Hello World!'"
+        #                         },
+        #                         {
+        #                             "name": "teamcity.step.mode",
+        #                             "value": "default"
+        #                         },
+        #                         {
+        #                             "name": "use.custom.script",
+        #                             "value": "true"
+        #                         }
+        #                     ]
+        #                 }
+        #             }
+        #         ]
+        #     }
+        # }
+
+
         # Получение токена
-        auth_response = requests.get(url = f"{BASE_URL}/authenticationTest.html?csrf)", auth=('admin', 'admin'))
-        csrf_token = auth_response.text
-        headers = {"X-TC-CSRF-Token": csrf_token}
-
-        #Подготовка данных
-        project_id ="simpleprojectID5"
-        conf_id = "simpleconfID2"
-        project_data={
-                        "parentProject": {
-                                    "locator": "_Root"
-                                        },
-                                    "name": "ProjectNameSimple5",
-                                    "id": project_id,
-                                    "copyAllAssociatedSettings": True
-                        }
-        build_data = {
-            "id": conf_id,
-            "name": "BuildConfName1",
-            "project": {
-                "id": project_id
-            },
-            "steps": {
-                "step": [
-                    {
-                        "name": "myCommandLineStep",
-                        "type": "simpleRunner",
-                        "properties": {
-                            "property": [
-                                {
-                                    "name": "script.content",
-                                    "value": "echo 'Hello World!'"
-                                },
-                                {
-                                    "name": "teamcity.step.mode",
-                                    "value": "default"
-                                },
-                                {
-                                    "name": "use.custom.script",
-                                    "value": "true"
-                                }
-                            ]
-                        }
-                    }
-                ]
-            }
-        }
-
-        build_start_data = {
-            "buildType": {
-                "id": conf_id
-            }
-        }
+        csrf_token = requester.send_request("GET", "/authenticationTest.html?csrf").text
+        requester._update_session_headers(**{"X-TC-CSRF-Token": csrf_token})
 
         # Создание проекта
-        create_responce = requests.post(url=f"{BASE_URL}/app/rest/projects", headers=headers, json=project_data)
+        create_responce = requester.send_request("POST", "/app/rest/projects", data=self.project_data)
         assert create_responce.status_code == 200, "Не удалось создать проект"
 
-        check_project = requests.get(url=f"{BASE_URL}/app/rest/projects/id:{project_id}", headers=headers)
+        check_project = requester.send_request("GET", f"/app/rest/projects/id:{self.create_project_id}")
         assert check_project.status_code == 200, "Не удалось проверить проект"
 
-        # Создание билда
-        create_build_responce = requests.post(url=f"{BASE_URL}/app/rest/buildTypes", headers=headers, json=build_data)
-        assert create_build_responce.status_code == 200, "Не удалось создать билд"
-
-        # Запуск билда
-        start_build_responce = requests.post(url=f"{BASE_URL}/app/rest/buildQueue", headers=headers, json=build_start_data)
-        assert start_build_responce.status_code == 200, "Не удалось запустить билд"
-
-        # Проверка статуса билда
-        checkbuildstatus_responce = requests.get(url=f"{BASE_URL}/app/rest/buildQueue?locator=buildType(id:{conf_id})", headers=headers)
-        assert checkbuildstatus_responce.status_code == 200, "Не удалось проверить билд"
+        # # Создание билда
+        # create_build_responce = requests.post(url=f"{BASE_URL}/app/rest/buildTypes", headers=headers, json=build_data)
+        # assert create_build_responce.status_code == 200, "Не удалось создать билд"
+        #
+        # # Запуск билда
+        # start_build_responce = requests.post(url=f"{BASE_URL}/app/rest/buildQueue", headers=headers, json=build_start_data)
+        # assert start_build_responce.status_code == 200, "Не удалось запустить билд"
+        #
+        # # Проверка статуса билда
+        # checkbuildstatus_responce = requests.get(url=f"{BASE_URL}/app/rest/buildQueue?locator=buildType(id:{conf_id})", headers=headers)
+        # assert checkbuildstatus_responce.status_code == 200, "Не удалось проверить билд"
 
         # Удаление проекта
-        delete_project = requests.delete(url=f"{BASE_URL}/app/rest/projects/id:{project_id}", headers=headers)
+        delete_project = requester.send_request("DELETE", f"/app/rest/projects/id:{self.create_project_id}", expected_status=204)
         assert delete_project.status_code == 204, "Не удалось удалить проект"
+
